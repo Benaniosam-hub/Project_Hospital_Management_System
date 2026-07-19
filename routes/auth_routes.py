f3f5e5229edb5ffdb52ecc4a79cc268373d8e4e0
Benaniosam <benaniosam008@gmail.com>
2026-07-19 13:21:08 +0530

    Changes took place in auth_routes

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from fastapi import APIRouter
 from controllers.auth_controller import register_controller, login_controller
 
-auth_bp = Blueprint('auth_bp', __name__)
+auth_bp = APIRouter()
 
-auth_bp.route('/register', methods= ['POST'])(register_controller)
-auth_bp.route('/login', methods=['POST'])(login_controller)
+@auth_bp.post('/register')
+async def register():
+    return await register_controller()
+
+@auth_bp.post('/login')
+async def login():
+    return await login_controller() 
