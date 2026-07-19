bc5f0e595d545f85ac6c17cef2eb45cc5aee8f10
Benaniosam <benaniosam008@gmail.com>
2026-07-19 15:24:23 +0530

    auth_controller plus other changes

--- routes/appointment_routes.py
+++ routes/appointment_routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-from controllers.appointment_controller import book_appointment_controller, get_doctor_schedule_controller
+from controllers.appointment_controller import book_appointment_controller, get_doctor_schedule_controller,AppointmentCreateSchema
 
 appointment_bp = APIRouter()
 
-@appointment_bp.post('/book')
-async def book_appointment_controller():
-    return await book_appointment_controller()
+@appointment_bp.post('/book', status_code=201)
+async def book_appointment(appointment_data: AppointmentCreateSchema):
+    return await book_appointment_controller(appointment_data)
 
 @appointment_bp.get('/doctor/{doctor_id}')
 async def get_doctor_schedule_controller(doctor_id: int):
--- routes/patient_routes.py
+++ routes/patient_routes.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from fastapi import APIRouter
 from controllers.patient_controller import register_patient_controller, get_patients_controller
 
-patient_bp = Blueprint('patient_bp',__name__)
+patient_bp = APIRouter()
 
-patient_bp.route('/register', methods=['POST'])(register_patient_controller)
-patient_bp.route('/',methods= ['GET'])(get_patients_controller)
+@patient_bp.post('/register')
+async def register_patient():
+    return await register_patient_controller()
+
+@patient_bp.get('/')
+async def get_patients():
+    return await get_patients_controller()
