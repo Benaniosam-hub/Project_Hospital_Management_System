fb3359b248dca1661e21e03fc27ee0c6b889ac71
Benaniosam <benaniosam008@gmail.com>
2026-07-19 13:16:23 +0530

    Changes took place in appointment_route.py

--- routes/appointment_routes.py
+++ routes/appointment_routes.py
@@ -1,8 +1,12 @@
-# routes/appointment_routes.py
-from flask import Blueprint
+from fastapi import APIRouter
 from controllers.appointment_controller import book_appointment_controller, get_doctor_schedule_controller
 
-appointment_bp = Blueprint('appointment_bp', __name__)
+appointment_bp = APIRouter()
 
-appointment_bp.route('/book', methods=['POST'])(book_appointment_controller)
-appointment_bp.route('/doctor/<int:doctor_id>', methods=['GET'])(get_doctor_schedule_controller)
+@appointment_bp.post('/book')
+async def book_appointment_controller():
+    return await book_appointment_controller()
+
+@appointment_bp.get('/doctor/{doctor_id}')
+async def get_doctor_schedule_controller(doctor_id: int):
+    return await get_doctor_schedule_controller(doctor_id)
