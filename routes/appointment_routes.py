50a794439aff7ea2baa1958b2d4036fe8bc39a65
Benaniosam <benaniosam008@gmail.com>
2026-07-20 15:57:02 +0530

    some changes

--- routes/appointment_routes.py
+++ routes/appointment_routes.py
@@ -8,5 +8,5 @@ async def book_appointment(appointment_data: AppointmentCreateSchema):
     return await book_appointment_controller(appointment_data)
 
 @appointment_bp.get('/doctor/{doctor_id}')
-async def get_doctor_schedule_controller(doctor_id: int):
+async def get_doctor_schedule(doctor_id: int):
     return await get_doctor_schedule_controller(doctor_id)
